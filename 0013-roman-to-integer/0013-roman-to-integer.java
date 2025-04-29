3c7a621cdc77ba74e9a41a8720fa60da4571a2f7
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2025-04-29 14:16:08 -0300

    Time: 32 ms (5.18%), Space: 45.7 MB (5.6%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,75 +1,29 @@
 class Solution {
     public int romanToInt(String s) {
-         int[] nums = s.chars().mapToObj(i -> (char) i).mapToInt(value -> {
-                return switch (value) {
-                    case 'I' -> 1;
-                    case 'V' -> 5;
-                    case 'X' -> 10;
-                    case 'L' -> 50;
-                    case 'C' -> 100;
-                    case 'D' -> 500;
-                    case 'M' -> 1000;
-                    default -> 0;
-                };
-         }).toArray();
+        String regex = "(?<=(?!IV|IX|XL|XC|CD|CM)[IVXLCDM])(?=[IVXLCDM])";
 
-         int sum = 0;
-         boolean passProx = false;
-         for (int i = 0; i < nums.length; i++) {
-                if (!passProx){
-                    if (nums[i] != 1 && nums[i] != 10 &&nums[i] != 100) sum += nums[i];
-                    if (nums[i] == 1 ) {
-                        if (i + 1 < nums.length){
-                            if (nums[i+1] == 5){
-                                sum += 4;
-                                passProx = true;
-                            }else if (nums[i+1] == 10){
-                                sum += 9;
-                                passProx = true;
-                            }else{
-                                sum += nums[i];
-                            }
-                            continue;
-                        }
-                        sum += nums[i];
-
-                     }
-                    if (nums[i] == 10 ) {
-                        if (i + 1 < nums.length){
-                            if (nums[i+1] == 50){
-                                sum += 40;
-                                passProx = true;
-                            }else if (nums[i+1] == 100){
-                                sum += 90;
-                                passProx = true;
-                            }else {
-                                sum += nums[i];
-
-                            }
-                            continue;
-                        }
-                        sum += nums[i];
-
-                    }
-                    if (nums[i] == 100 ) {
-                        if (i + 1 < nums.length){
-                            if (nums[i+1] == 500){
-                                sum += 400;
-                                passProx = true;
-                            }else if (nums[i+1] == 1000){
-                                sum += 900;
-                                passProx = true;
-                            }else {
-                                sum += nums[i];
-                            }
-                            continue;
-                        }
-                        sum += nums[i];
-                    }
-                } else {
-                    passProx = false;
-                }
-         }
-         return sum;
+        return Arrays.stream(s.split(regex))
+                .mapToInt(s1 -> {
+                    return switch (s1) {
+                        case "IV" -> 4;
+                        case "IX" -> 9;
+                        case "XL" -> 40;
+                        case "XC" -> 90;
+                        case "CD" -> 400;
+                        case "CM" -> 900;
+                        default -> s1.chars().mapToObj(i -> (char) i).mapToInt(operand -> {
+                            return switch (operand) {
+                                case 'I' -> 1;
+                                case 'V' -> 5;
+                                case 'X' -> 10;
+                                case 'L' -> 50;
+                                case 'C' -> 100;
+                                case 'D' -> 500;
+                                case 'M' -> 1000;
+                                default -> 0;
+                            };
+                        }).sum();
+                    };
+                }).sum();
     }
 }
