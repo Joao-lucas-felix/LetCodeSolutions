16cfa6caa6423a2f9a0ea0a42b147c76e52fc35b
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2025-05-22 11:39:31 -0300

    Time: 8 ms (11.86%), Space: 42 MB (33.59%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.kt
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.kt
@@ -1,20 +1,17 @@
 class Solution {
     fun strStr(haystack: String, needle: String): Int {
-               val iter = haystack.iterator()
+        val iter = haystack.iterator()
         var index = 0
         while (iter.hasNext()){
 
             if (index + needle.length <= haystack.length){
-                var isEqual = true
-                for (i in 0..needle.length-1){
-                    if (haystack[index + i] == needle[i]){ continue }
-                    isEqual = false
+                if (needle == haystack.substring(index, index+ needle.length)) {
+                    return index
                 }
-                if(isEqual) return index;
             }
             index += 1;
             iter.next()
         }
-        return  -1 
+        return  -1
     }
 }
